b485582d8bf201102b86762197c595ad116c5366
Hasan Setiawan <hasan.setiawan@kodefox.com>
2016-10-01 01:21:19 +0700

    add landing page

---
+++ landing/index.php
@@ -0,0 +1,193 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" type="image/png" href="../../favicon.png"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Agrotech E-Commerce</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/sticky-footer.css" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <!-- Fixed navbar -->
+   <nav class="navbar navbar-default navbar-fixed-top">
+     <div class="container">
+       <div class="navbar-header">
+         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+         </button>
+         <a class="navbar-brand" href="#">AGROTECH E-COMMERCE</a>
+       </div>
+       <div id="navbar" class="collapse navbar-collapse">
+         <ul class="nav navbar-nav">
+           <li class="active"><a href="#">Home</a></li>
+           <li><a href="#about">PRODUCT</a></li>
+           <li><a href="#contact">NEWS</a></li>
+           <li class="dropdown">
+             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+             <ul class="dropdown-menu">
+               <li><a href="#">Action</a></li>
+               <li role="separator" class="divider"></li>
+               <li class="dropdown-header">Nav header</li>
+               <li><a href="#">Separated link</a></li>
+               <li><a href="#">One more separated link</a></li>
+             </ul>
+           </li>
+         </ul>
+       </div><!--/.nav-collapse -->
+     </div>
+   </nav>
+
+   <!-- Begin page content -->
+   <div class="container">
+     <div class="page-header">
+       <h1>Products</h1>
+     </div>
+
+     <div class="panel panel-success">
+       <div class="panel-heading">
+         <h3 class="panel-title">Karet Plus</h3>
+       </div>
+       <div class="panel-body">
+         <div class="col-md-12">
+           <div class="pull-left">
+             <img src="assets/img/karetplus-product.png" class="img-responsive" />
+           </div>
+           <div class="pull-left">
+             <p>
+               Keajaiban yang dapat Memperpendek Masa Gugur Daun / Trek
+               Maka daun akan lebih cepat tumbuh dan secara umum daun terlihat lebih lebar dan tebal. Aman digunakan pada saat musim gugur daun untuk kebun dengan pemupukan berimbang setiap tahunnya, sehingga karetPLUS aman digunakan selama 12 bulan.
+             </p>
+           </div>
+         </div>
+       </div>
+       <div class="panel-footer"><a href="#"><button type="button" class="btn btn-primary">Beli</button></a></div>
+     </div>
+
+     <div class="panel panel-success">
+       <div class="panel-heading">
+         <h3 class="panel-title">Karet Plus</h3>
+       </div>
+       <div class="panel-body">
+         <div class="col-md-12">
+           <div class="pull-left">
+             <img src="assets/img/karetplus-product.png" class="img-responsive" />
+           </div>
+           <div class="pull-left">
+             <p>
+               Keajaiban yang dapat Memperpendek Masa Gugur Daun / Trek
+               Maka daun akan lebih cepat tumbuh dan secara umum daun terlihat lebih lebar dan tebal. Aman digunakan pada saat musim gugur daun untuk kebun dengan pemupukan berimbang setiap tahunnya, sehingga karetPLUS aman digunakan selama 12 bulan.
+             </p>
+           </div>
+         </div>
+       </div>
+       <div class="panel-footer"><a href="#"><button type="button" class="btn btn-primary">Beli</button></a></div>
+     </div>
+
+     <div class="panel panel-success">
+       <div class="panel-heading">
+         <h3 class="panel-title">Karet Plus</h3>
+       </div>
+       <div class="panel-body">
+         <div class="col-md-12">
+           <div class="pull-left">
+             <img src="assets/img/karetplus-product.png" class="img-responsive" />
+           </div>
+           <div class="pull-left">
+             <p>
+               Keajaiban yang dapat Memperpendek Masa Gugur Daun / Trek
+               Maka daun akan lebih cepat tumbuh dan secara umum daun terlihat lebih lebar dan tebal. Aman digunakan pada saat musim gugur daun untuk kebun dengan pemupukan berimbang setiap tahunnya, sehingga karetPLUS aman digunakan selama 12 bulan.
+             </p>
+           </div>
+         </div>
+       </div>
+       <div class="panel-footer"><a href="#"><button type="button" class="btn btn-primary">Beli</button></a></div>
+     </div>
+
+     <div class="panel panel-success">
+       <div class="panel-heading">
+         <h3 class="panel-title">Karet Plus</h3>
+       </div>
+       <div class="panel-body">
+         <div class="col-md-12">
+           <div class="pull-left">
+             <img src="assets/img/karetplus-product.png" class="img-responsive" />
+           </div>
+           <div class="pull-left">
+             <p>
+               Keajaiban yang dapat Memperpendek Masa Gugur Daun / Trek
+               Maka daun akan lebih cepat tumbuh dan secara umum daun terlihat lebih lebar dan tebal. Aman digunakan pada saat musim gugur daun untuk kebun dengan pemupukan berimbang setiap tahunnya, sehingga karetPLUS aman digunakan selama 12 bulan.
+             </p>
+           </div>
+         </div>
+       </div>
+       <div class="panel-footer"><a href="#"><button type="button" class="btn btn-primary">Beli</button></a></div>
+     </div>
+
+     <div class="panel panel-success">
+       <div class="panel-heading">
+         <h3 class="panel-title">Karet Plus</h3>
+       </div>
+       <div class="panel-body">
+         <div class="col-md-12">
+           <div class="pull-left">
+             <img src="assets/img/karetplus-product.png" class="img-responsive" />
+           </div>
+           <div class="pull-left">
+             <p>
+               Keajaiban yang dapat Memperpendek Masa Gugur Daun / Trek
+               Maka daun akan lebih cepat tumbuh dan secara umum daun terlihat lebih lebar dan tebal. Aman digunakan pada saat musim gugur daun untuk kebun dengan pemupukan berimbang setiap tahunnya, sehingga karetPLUS aman digunakan selama 12 bulan.
+             </p>
+           </div>
+         </div>
+       </div>
+       <div class="panel-footer"><a href="#"><button type="button" class="btn btn-primary">Beli</button></a></div>
+     </div>
+
+     <div class="panel panel-success">
+       <div class="panel-heading">
+         <h3 class="panel-title">Karet Plus</h3>
+       </div>
+       <div class="panel-body">
+         <div class="col-md-12">
+           <div class="pull-left">
+             <img src="assets/img/karetplus-product.png" class="img-responsive" />
+           </div>
+           <div class="pull-left">
+             <p>
+               Keajaiban yang dapat Memperpendek Masa Gugur Daun / Trek
+               Maka daun akan lebih cepat tumbuh dan secara umum daun terlihat lebih lebar dan tebal. Aman digunakan pada saat musim gugur daun untuk kebun dengan pemupukan berimbang setiap tahunnya, sehingga karetPLUS aman digunakan selama 12 bulan.
+             </p>
+           </div>
+         </div>
+       </div>
+       <div class="panel-footer"><a href="#"><button type="button" class="btn btn-primary">Beli</button></a></div>
+     </div>
+
+   </div>
+
+   <footer class="footer">
+     <div class="container">
+       <p class="text-muted">&copy;AGROTECH E-COMMERCE 2016</p>
+     </div>
+   </footer>
+  </body>
+</html>
+ <script src="assets/js/j-query.js"></script>
+ <script src="assets/js/bootstrap.min.js"></script>
